70040f6faa54f14630c35bddb5438f8a77bf266b
ANAJULIA <leliasallesh@gmail.com>
2026-03-09 22:08:15 -0300

    update

--- Source/SonobusPluginEditor.cpp
+++ Source/SonobusPluginEditor.cpp
@@ -46,7 +46,7 @@ enum {
     PeerLayoutRadioGroupId = 1
 };
 
-#define SONOBUS_SCHEME "sonobus"
+#define SONOBUS_SCHEME "studiomaster"
 
 using namespace SonoAudio;
 
@@ -354,7 +354,7 @@ SonobusAudioProcessorEditor::SonobusAudioProcessorEditor (SonobusAudioProcessor&
     mTitleLabel->addMouseListener(this, false);
 
     mTitleImage = std::make_unique<ImageComponent>("title");
-    mTitleImage->setImage(ImageCache::getFromMemory(BinaryData::sonobus_logo_96_png, BinaryData::sonobus_logo_96_pngSize));
+    mTitleImage->setImage(ImageCache::getFromMemory(BinaryData::studiomaster_logo6.sm_png, BinaryData::studiomaster_logo6.sm_pngSize));
     mTitleImage->setInterceptsMouseClicks(true, false);
     mTitleImage->addMouseListener(this, false);
 
